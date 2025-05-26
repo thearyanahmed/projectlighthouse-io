ee3d820ba36c84d9a70e41bc04f9af8c9d8056e9
Aryan Ahmed <thearyanahmed@gmail.com>
2025-05-26 09:30:59 +0100

    Updated initial schema

--- initial_db_planning.sql
+++ initial_db_planning.sql
@@ -4,7 +4,7 @@ CREATE TABLE users {
     email VARCHAR(100) UNIQUE NOT NULL,
 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
     google_id VARCHAR(100) UNIQUE NULL,
     google_access_token VARCHAR(255) NULL,
@@ -13,8 +13,6 @@ CREATE TABLE users {
     github_id VARCHAR(100) UNIQUE NULL,
     github_access_token VARCHAR(255) NULL,
     github_refresh_token VARCHAR(255) NULL,
-
-    # need to have fields for bio and stuff
 }
 
 CREATE TABLE courses {
@@ -22,9 +20,10 @@ CREATE TABLE courses {
     name VARCHAR(100) NOT NULL,
     description TEXT NOT NULL,
     slug VARCHAR(100) UNIQUE NOT NULL,
+    locale smallint NOT NULL, -- default to 0 : English, 1: Bangla
 
-    locale smallint NOT NULL, # default to 0 : should be english
-
+    course_thumbnail VARCHAR(150) NULL,
+    
     # need to have SEO related fields
     meta_name VARCHAR(100) NULL,
     meta_description TEXT NULL,
@@ -32,12 +31,7 @@ CREATE TABLE courses {
     meta_image VARCHAR(255) NULL,
     meta_url VARCHAR(255) NULL,
 
-    # need to have view stats
-    public_visits INT DEFAULT 0,
-    total_course_page_read INT DEFAULT 0, # number of times an page was read
-
     price DECIMAL(10, 2) DEFAULT 0.00,
-
     page_layout VARCHAR(100) DEFAULT 'default',
 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -47,35 +41,39 @@ CREATE TABLE courses {
 # use hardcoded const for categories and tags
 # use small integer for those in the db
 
+
 # coure categories is a many to many relationship
 CREATE TABLE course_categories {
     course_id INT NOT NULL,
-    category_id INT NOT NULL, # not in db, but const 
+    category_id INT NOT NULL, -- not in db, but const 
 }
 
 # tags are same as well, use hardcoded struct
 CREATE TABLE course_tags {
     course_id INT NOT NULL,
-    tag_id INT NOT NULL, # not in db, but const
+    tag_id INT NOT NULL, -- not in db, but const
 }
 
 CREATE TABLE course_chapters {
     id SERIAL PRIMARY KEY,
     course_id INT NOT NULL,
     name VARCHAR(100) NOT NULL,
+    description VARCHAR(500) NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
     FOREIGN KEY (course_id) REFERENCES courses(id)
 }
 
-CREATE TABLE contents {
+CREATE TABLE lessons {
     id SERIAL PRIMARY KEY,
     course_id INT NOT NULL,
     chapter_id INT NULL,
     name VARCHAR(100) NOT NULL,
-    embed_content VARCHAR(1000) NULL, # having this null means it is not a video
+    thumbnail VARCHAR(255) NULL,
+    media_content VARCHAR(1000) NULL, # nullable, could be audio, could be video
     content TEXT NOT NULL,
+    
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
@@ -83,14 +81,59 @@ CREATE TABLE contents {
     meta_description TEXT NULL,
     meta_keywords TEXT NULL,
     meta_image VARCHAR(255) NULL,
-    meta_url VARCHAR(255) NULL,
+    
     page_layout VARCHAR(100) DEFAULT 'default',
+
+    read_time VARCHAR(10) NULL,
+    watch_time VARCHAR(10) NULL,
+    
     FOREIGN KEY (course_id) REFERENCES courses(id),
     FOREIGN KEY (chapter_id) REFERENCES course_chapters(id)
 }
 
-CREATE TABLE course_tags {
-    content_id INT NOT NULL,
-    tag_id INT NOT NULL, # not in db, but const
+CREATE TABLE lesson_tags {
+    lesson_id INT NOT NULL,
+    tag_id INT NOT NULL, -- not in db, but const
 }
 
+CREATE TABLE comments {
+    id SERIAL PRIMARY KEY,
+
+    user_id INT NOT NULL,
+    lesson_id INT NOT NULL,
+    content TEXT NOT NULL,
+    
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (lesson_id) REFERENCES lessons(id),
+    
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+}
+
+CREATE TABLE questions {
+    id SERIAL PRIMARY KEY,
+    
+    user_id INT NOT NULL,
+    lesson_id INT NOT NULL,
+    content TEXT NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (lesson_id) REFERENCES lessons(id),
+    
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+}
+
+CREATE TABLE answers {
+    id SERIAL PRIMARY KEY,
+    
+    user_id INT NOT NULL,
+    lesson_id INT NOT NULL,
+    content TEXT NOT NULL,
+    
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (lesson_id) REFERENCES lessons(id),
+    
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+}
