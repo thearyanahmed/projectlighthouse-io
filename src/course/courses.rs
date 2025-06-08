1eb61399b616b37b4ef70f8c7af6d37f240d9351
Aryan <paryan@digitalocean.com>
2025-06-08 18:23:46 +0100

    Move storer/course to it's own module

--- src/course/courses.rs
+++ src/course/courses.rs
@@ -1,3 +1,51 @@
+use sqlx::types::chrono;
+use sqlx::{PgPool, Result};
+
+pub struct Course {
+    pub id: i32,
+    pub name: String,
+    pub slug: String,
+    pub description: String,
+    pub thumbnail: Option<String>,
+
+    pub seo_title: Option<String>,
+    pub seo_description: Option<String>,
+    pub seo_keywords: Option<String>,
+    pub seo_image: Option<String>,
+
+    pub published_at: Option<chrono::NaiveDateTime>,
+    pub created_at: Option<chrono::NaiveDateTime>,
+    pub updated_at: Option<chrono::NaiveDateTime>,
+    pub deleted_at: Option<chrono::NaiveDateTime>,
+}
+
+pub async fn get_courses(pool: &PgPool) -> Result<Vec<Course>> {
+    let courses = sqlx::query_as!(
+        Course,
+        r#"
+        SELECT
+            id,
+            name,
+            slug,
+            description,
+            thumbnail,
+            seo_title,
+            seo_description,
+            seo_keywords,
+            seo_image,
+            published_at,
+            created_at,
+            updated_at,
+            deleted_at
+        FROM courses
+        "#
+    )
+    .fetch_all(pool)
+    .await?;
+
+    Ok(courses)
+}
+
 pub fn hello_world() {
     println!("Hello, world!");
 }
