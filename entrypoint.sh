008400811476bc021a0a89b8370ce04e03e13738
Aryan <paryan@digitalocean.com>
2025-06-24 20:18:05 +0100

    Listen to the world

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@ set -e
 
 mkdir -p /app/config
 cat > /app/config/base.yml <<EOF
-base_url: "${BASE_URL:-http://localhost:8080}"
+base_url: "${BASE_URL:-http://0.0.0.0:8080}"
 port: ${PORT:-8081}
-host: "${HOST:-127.0.0.1}"
+host: "${HOST:-0.0.0.0}"
 database:
   driver: "${DB_DRIVER:-postgres}"
   host: "${DB_HOST:-localhost}"
